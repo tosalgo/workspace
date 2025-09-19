790ebbe92ade71336bb566d3f4e121d95f115441
tosalgo <tosalgoo@gmail.com>
2025-09-19 10:38:04 +0900

    feat: GitHub Pages 배포 설정 추가

---
+++ storybook-static/assets/Header.stories-FfMjPJfr.js
@@ -0,0 +1,7 @@
+import{H as a}from"./Header-DUWWwk5e.js";import"./iframe-CHAtGI7h.js";import"./preload-helper-D9Z9MdNV.js";import"./Button-DaPTVOV7.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,m as default};
